02c0f5e9382915f5eed7ad8420fd7dcadcfaef8a
Szabolcs Balogh <bszabolcs@gmail.com>
2015-04-17 14:03:20 +0300

    Fix downloading libogg and libvorbis

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -17,7 +17,7 @@ wget -O- http://downloads.sourceforge.net/project/libpng/libpng16/older-releases
 wget -O- http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz | tar xz
 wget -O- http://fribidi.org/download/fribidi-0.19.6.tar.bz2 | tar xj
 wget -O- http://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz | tar xz
-wget -O- http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.xz | tar xz
-wget -O- http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz | tar xz
+wget -O- http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz | tar xz
+wget -O- http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz | tar xz
 wget -O- http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2 | tar xj
 echo "============================================"
